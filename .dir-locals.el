e62c0127702c4a8b21e6bf41ef7b66cc98c64f80
M <38927629+YZmarkS@users.noreply.github.com>
2026-08-25 22:32:29 -0400

    Pull from other branch

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,8 @@
 ((nil . ((eval . (setq-local
-                  org-roam-directory (file-name-concat
-				      (expand-file-name (locate-dominating-file
-                                                         default-directory ".dir-locals.el")))))
+                  org-roam-directory (expand-file-name (locate-dominating-file
+                                                        default-directory ".dir-locals.el"))))
          (eval . (setq-local
                   org-roam-db-location (expand-file-name "org-roam.db"
                                                          org-roam-directory)))
-	 (eval . (org-roam-db-autosync-mode))
+	 ;; (eval . (org-roam-db-autosync-mode))
 	 )))
