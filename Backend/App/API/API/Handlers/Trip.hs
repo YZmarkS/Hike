5d3170a4b037b26ccf3bcc5f63e58314a74a92d7
M <38927629+YZmarkS@users.noreply.github.com>
2026-09-02 22:04:40 -0400

    Learn about persistent
    We learned some inner working of `persistent`
    We used `do` notation to write cleaner code
    We learned to use `catch` for specific errors

--- Backend/App/API/API/Handlers/Trip.hs
+++ Backend/App/API/API/Handlers/Trip.hs
@@ -14,59 +14,51 @@ import Database.Esqueleto.Experimental
 import Servant
 import Model
 
-postTripServer :: HikeAuthResult -> Trip -> AppM TripId
-postTripServer hikeAuthResult trip = do
+postTripHandler :: HikeAuthResult -> Trip -> AppM TripId
+postTripHandler hikeAuthResult trip = do
   { userId <- extractUserId hikeAuthResult
   ; pool <- asks id
   ; let canonicalTrip = trip { tripOwnerId = userId }
-  ; let action = do { maybeInsertTrip <- P.insertUnique canonicalTrip
-                    ; case maybeInsertTrip of
-                        Nothing -> return Nothing
-                        Just tripId -> do
-                          { maybeMembershipId <- P.insertUnique $ Membership userId tripId
-                          ; case maybeMembershipId of
-                              Nothing -> return Nothing
-                              Just _ -> return $ Just tripId } }
-
-  ; maybeTripId <- liftIO $ runSqlPool action pool
+  ; let action = do { Just tripId <- P.insertUnique canonicalTrip
+                    ; Just _ <- P.insertUnique $ Membership userId tripId
+                    ; return $ Just tripId }
+  ; maybeTripId <- liftIO $ catch (runSqlPool action pool)
+                                  (\(_ :: IOError) -> return Nothing)
   ; case maybeTripId of
       Nothing -> throwError err409
       Just tripId -> return tripId
   }
 
-
-  -- ; insertTripResult <- liftIO $ runSqlPool (P.insertBy canonicalTrip) pool
-  -- ; tripId <- case insertTripResult of
-  --               Left trip' -> let errBody = if tripName trip == tripName (entityVal trip')
-  --                                           then "Trip with same name already exists"
-  --                                           else append
-  --                                                    "Cannot insert due to trip id: "
-  --                                                    (fromString $ show $ entityKey trip' ) -- is there no better way?
-  --                             in throwError $ err409 { errBody }
-  --               Right newTripId -> return newTripId
-  -- ; insertMembershipResult <- liftIO $ runSqlPool (P.insertBy $ Membership userId tripId) pool
-  -- ;
-
-getAllTripsServer :: AppM [Entity Trip]
-getAllTripsServer = do
+getAllTripsHandler :: AppM [Entity Trip]
+getAllTripsHandler = do
   { pool <- asks id
   ; liftIO $ runSqlPool (P.selectList [] []) pool }
 
-getUserTripsServer :: HikeAuthResult -> AppM [Entity Trip]
-getUserTripsServer hikeAuthResult = do
+getUserTripsHandler :: HikeAuthResult -> AppM [Entity Trip]
+getUserTripsHandler hikeAuthResult = do
   { userId <- extractUserId hikeAuthResult
   ; pool <- asks id
   ; liftIO $ runSqlPool (do { select $ do
                                 { (membership :& trip) <-
-                                      from $ table @Membership `InnerJoin` table @Trip
-                                               `on` \(membership :& trip) -> membership ^. MembershipTripId ==. trip ^. TripId
+                                      from $ table @Membership
+                                               `InnerJoin` table @Trip
+                                               `on` \(membership :& trip) ->
+                                                   membership ^. MembershipTripId ==. trip ^. TripId
                                 ; where_ (membership ^. MembershipUserId ==. val userId)
                                 ; return trip }
                             } ) pool
   }
 
-deleteTripServer :: HikeAuthResult -> TripId -> AppM String
-deleteTripServer hikeAuthResult tripId = do
+patchRenameHandler :: HikeAuthResult -> TripId -> Text -> AppM String
+patchRenameHandler hikeAuthResult tripId newName = do
+  { userId <- extractUserId hikeAuthResult
+  ; userId `isOwnerOf` tripId
+  ; pool <- asks id
+  ; liftIO $ runSqlPool (P.update tripId [ TripName P.=. newName ]) pool
+  ; return "Renamed" }
+
+deleteTripHandler :: HikeAuthResult -> TripId -> AppM String
+deleteTripHandler hikeAuthResult tripId = do
   { userId <- extractUserId hikeAuthResult
   ; isOwnerOf userId tripId
   ; pool <- asks id
